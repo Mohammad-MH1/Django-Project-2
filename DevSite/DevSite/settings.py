bb4290fec688a8d7aa39afdd9ce2b565d165be3f
Mohammad Mohammadi <93423683+Mohammad-MH1@users.noreply.github.com>
2022-01-19 15:54:14 +0330

    Update settings.py

--- DevSite/DevSite/settings.py
+++ DevSite/DevSite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fbpm&+p$(0_%4^ie2_dr0p$f2$qr9z4rd6y6h54_v=%_t-ho)b'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -169,8 +169,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'backend.work1@gmail.com'
-EMAIL_HOST_PASSWORD = 'yydxadghhxmiwisy'
+EMAIL_HOST_USER = os.environ.get("USER")
+EMAIL_HOST_PASSWORD = os.environ.get("PASS")
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
